d5bc1af4973b69b6dd1bee5de22f04164bbe54f5
jcHernande2 <hhjuan27@gmail.com>
2024-12-19 17:25:50 -0600

    init commit

---
+++ SqlGenerator/Controllers/XlsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SqlGenerator.Controllers
+{
+    public class XlsController : Controller
+    {
+        // GET: XlsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: XlsController/sql
+        [HttpGet]
+        [Route("XlsController/sql")]
+        public ActionResult GetSql()
+        {
+            try
+            {
+                var request = HttpContext.Request;
+                // Check if the request contains multipart/form-data.
+               /* if (!Request.HttpContext.Content.IsMimeMultipartContent())
+                {
+                    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                }
+
+                string root = HttpContext.Current.Server.MapPath("~/App_Data");
+                var provider = new MultipartFormDataStreamProvider(root);
+
+                try
+                {
+                    // Read the form data.
+                    await Request.Content.ReadAsMultipartAsync(provider);
+
+                    // This illustrates how to get the file names.
+                    foreach (MultipartFileData file in provider.FileData)
+                    {
+                        Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                        Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (System.Exception e)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+                }
+                }*/
+                return this.Ok("");
+            }
+            catch {
+                return this.BadRequest();
+            }
+        }
+
+    }
+}
